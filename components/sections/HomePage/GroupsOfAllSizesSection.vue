75f5dafe91acc8f96c54754658ddc7fccf8c700e
Donavan Jones <donavan@businessbenefitalliance.com>
2024-03-02 10:48:28 -0500

    first commit

---
+++ components/sections/HomePage/GroupsOfAllSizesSection.vue
@@ -0,0 +1,45 @@
+<template>
+  <BasicSection class="container mx-auto mt-20">
+    <GridTwoColumns>
+      <div class="px-4 sm:px-8">
+        <NuxtImg
+          format="webp"
+          src="/images/happy-kids.jpg"
+          sizes="sm:217px md:323px lg:376px xl:504px xxl:632px 2xl:700px"
+          alt="Happy Kids"
+          quality="80"
+          loading="lazy"
+        />
+      </div>
+      <div class="px-4 sm:px-8">
+        <HeadlineTwo>{{ IntroText.Headline }}</HeadlineTwo>
+        <ParagraphDescription>
+          {{ IntroText.description }}
+        </ParagraphDescription>
+      </div>
+    </GridTwoColumns>
+  </BasicSection>
+</template>
+
+<script>
+export default {
+  name: "Ideal Customers Section",
+  props: {
+    title: String,
+    description: String,
+    mediaFirst: Boolean,
+    subTitle: String,
+  },
+  data() {
+    return {
+      IntroText: {
+        SubHeading:
+          "To Expand Your Business, Increase Online Leads, Calls, And Revenue",
+        Headline: "Groups of All Sizes",
+        description:
+          "Every trip comes with its own unique needs. Good Time Travel's charter services are designed to provide a comfortable, smooth, and relaxing ride. Whether multiple pick-up locations or routine options, we will work hard to ensure your traveling needs are met to the best of our ability.",
+      },
+    };
+  },
+};
+</script>
---
+++ components/sections/HomePage/HomepageHeroSection.vue
@@ -0,0 +1,93 @@
+<template>
+  <SectionContainer class="text-center">
+    <GridTwoColumns>
+      <div>
+        <NuxtImg
+          preload
+          format="webp"
+          src="/img/charter-bus.jpg"
+          alt="Happy Kids"
+          quality="80"
+          width="910"
+        />
+      </div>
+      <div>
+        <ParagraphSubHeadingHero>
+          {{ IntroText.SubHeading }}
+        </ParagraphSubHeadingHero>
+        <HeadlineOneHero>
+          {{ IntroText.Heading }}
+        </HeadlineOneHero>
+        <ParagraphDescriptionHero>
+          {{ IntroText.Description }}
+        </ParagraphDescriptionHero>
+        <CardDeckThreeColumns class="hidden sm:block">
+          <Card
+            v-for="Feature in Featured"
+            :key="Feature.id"
+            :title="Feature.title"
+            :description="Feature.description"
+          />
+        </CardDeckThreeColumns>
+
+        <CtaButton :to="CTA.to"> {{ CTA.text }} </CtaButton>
+      </div>
+    </GridTwoColumns>
+  </SectionContainer>
+</template>
+
+<script>
+export default {
+  name: "HomepageHeroSection",
+  data() {
+    return {
+      IntroText: {
+        SubHeading:
+          "Embark on a Journey of Comfort, Convenience, and Exceptional Service",
+        Heading:
+          "Explore Boundless Adventures with Good Time Travel Bus Charters",
+        Description:
+          "At Good Time Travel, we redefine the travel experience with our premier bus charter services. From scenic road trips to group excursions, we prioritize your comfort and satisfaction every step of the way. With our luxurious amenities, expert staff, and commitment to excellence, your journey with us is guaranteed to be memorable and stress-free.",
+      },
+      Featured: [
+        {
+          id: 1,
+          title: "Unmatched Comfort",
+          description:
+            "Travel in style and relaxation with our luxurious reclining seats and spacious interiors, ensuring a smooth and enjoyable ride for all passengers.",
+        },
+        {
+          id: 2,
+          title: "Convenient Amenities",
+          description:
+            "Stay connected and entertained throughout your journey with onboard flat screen monitors and charging ports, providing entertainment and power at your fingertips.",
+        },
+        {
+          id: 3,
+          title: "Expert Service",
+          description:
+            "Our dedicated and friendly staff are committed to providing top-notch service, from assistance with loading luggage to ensuring a safe and pleasant travel experience for all.",
+        },
+      ],
+      CTA: {
+        to: "/get-started",
+        text: "Get Started Now!",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.image-container {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.full-width-height {
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* Ensure the image covers the entire container */
+}
+</style>
