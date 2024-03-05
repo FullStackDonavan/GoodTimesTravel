a8551479f5d4ec9ff2e90ffed02056a148e31f3d
FullStackDonavan <donavan@businessbenefitalliance.com>
2024-03-05 13:25:05 -0500

    Local Business update 43

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,7 +40,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     private: {
       stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-      db: process.env.POSTGRES_URL,
+      db: process.env.POSTGRES_PRISMA_URL,
     },
     public: {
       GHL_API_KEY: process.env.GHL_API_KEY,
