334e7024c4c83eed96cd5fe7b707943ffa7e3167
FullStackDonavan <donavancjones@gmail.com>
2024-04-05 15:22:34 -0400

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -73,6 +73,9 @@ export default defineNuxtConfig({
   robots: {
     enabled: true,
   },
+  seo: {
+    redirectToCanonicalSiteUrl: true
+  }
   schemaOrg: {
     identity: {
       type: 'Organization',
